efae3f754e2d79b14617ddefeb458f45ef6e10e2
Daniil Popov <daniil.popov.office@gmail.com>
2017-03-13 02:54:21 +0300

    Rewrite Server part to support chat

--- server/Server.cpp
+++ server/Server.cpp
@@ -9,6 +9,8 @@ Server::Server (int portNumber)
 {
 	port = portNumber;
 	int yes = 1;
+	FD_ZERO(&master);
+	FD_ZERO(&read_fds);
 
 	if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1)
 		throw std::runtime_error("Could not create socket");
@@ -24,7 +26,10 @@ Server::Server (int portNumber)
 		std::runtime_error("Could not bind socket to port");
 	if (listen(sockfd, 10) == -1)
 		throw std::runtime_error("Could not start listening");
-	
+
+	FD_SET(sockfd, &master);
+	fdmax = sockfd;
+
 	sa.sa_handler = sigchld_handler;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_RESTART;
@@ -39,24 +44,41 @@ void Server::sigchld_handler(int s)
 
 void Server::run()
 {
+	int i, j, nbytes, addrlen;
 	std::cout << "Server launched on port " << port << std::endl;
-	while(1) 
-	{ 
-		int sin_size = sizeof(struct sockaddr_in);
-        if ((new_fd = accept(sockfd, (struct sockaddr *)&cli_addr, (unsigned int*)&sin_size)) == -1)
-			throw std::runtime_error("Could not accept connection");
-		std::cout << "Connect caught" << std::endl;
-        printf("server: got connection from %s\n", inet_ntoa(cli_addr.sin_addr));
-        if (!fork()) 
-		{
-			std::cout << "Forked!" << std::endl;
-            close(sockfd);
-            if (send(new_fd, "Hello, world!\n", 14, 0) == -1)
-				throw std::runtime_error("send");
-            close(new_fd);
-            exit(0);
-        }
-        close(new_fd);
-    }
+	for(;;) {
+		read_fds = master; 
+		if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1)
+			throw std::runtime_error("Could not poll for incoming connections (select)");
+		for(i = 0; i <= fdmax; i++) {
+			if (FD_ISSET(i, &read_fds)) { // we got one!!
+				if (i == sockfd) {
+					addrlen = sizeof(cli_addr);
+					if ((new_fd = accept(sockfd, (struct sockaddr *)&cli_addr, (unsigned int*)&addrlen)) == -1) 
+						throw std::runtime_error("Could not accept new connection");
+							else 
+							{
+								FD_SET(new_fd, &master); // add to master set
+								if (new_fd > fdmax)
+									fdmax = new_fd;
+								printf("selectserver: new connection from %s on " "socket %d\n", inet_ntoa(cli_addr.sin_addr), new_fd);
+							}
+				}
+				if ((nbytes = recv(i, buffer, sizeof(buffer), 0)) <= 0) 
+				{
+					if (nbytes == 0)
+						printf("selectserver: socket %d hung up\n", i);
+					else
+						throw std::runtime_error("Could not receive correctly");
+					close(i);
+					FD_CLR(i, &master);
+				}
+				else
+					for(j = 0; j <= fdmax; j++)
+						if (FD_ISSET(j, &master) && (j != sockfd && j != i) && (send(j, buffer, nbytes, 0) == -1))
+									throw std::runtime_error("Could not send data to client");
+			}
+		}
+	}
 }
 }
--- server/Server.hpp
+++ server/Server.hpp
@@ -14,6 +14,7 @@ class Server
 {
 public:
 	static const int DEFAULT_PORT = 4567;
+	static const int MAX_MSG_SIZE = 4096;
 
 	Server (int portNumber=DEFAULT_PORT);
 
@@ -21,9 +22,12 @@ public:
 
 private:
 	int sockfd, new_fd, port;
-	char buffer[256];
+	char buffer[4096];
     struct sockaddr_in serv_addr, cli_addr;
 	struct sigaction sa;
+	fd_set master;
+	fd_set read_fds; 
+	int fdmax;
 
 	static void sigchld_handler(int s);
 };
